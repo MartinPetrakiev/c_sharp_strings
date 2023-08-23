cc5ae9edf20e30be49ff32fc65ea11fd3e543293
martinmp <martomsl@gmail.com>
2023-08-23 14:48:04 +0300

    to task 7

---
+++ Parse_tags/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static string ParseupcaseTagsIteratively(string inputString)
+    {
+        int indexOpeningTag = 0;
+        int indexClosingTag = 0;
+        int end = inputString.Length;
+        int start = 0;
+        int countCharactersToExamine = 0;
+
+        while ((start <= end) && (indexOpeningTag > -1) && (indexClosingTag > -1))
+        {
+            countCharactersToExamine = end - start;
+            indexOpeningTag = inputString.IndexOf("<upcase>", start, countCharactersToExamine);
+
+            if (indexOpeningTag == -1)
+            {
+                break;
+            }
+
+            start = indexOpeningTag + 8;
+            countCharactersToExamine = end - start;
+
+            indexClosingTag = inputString.IndexOf("</upcase>", start, countCharactersToExamine);
+
+            if (indexClosingTag == -1)
+            {
+                break;
+            }
+
+            countCharactersToExamine = indexClosingTag - start;
+
+            string currentSubstring = inputString.Substring(start, indexClosingTag - start);
+            string currentSubstringToUpper = currentSubstring.ToUpper();
+
+            inputString = inputString.Replace("<upcase>" + currentSubstring + "</upcase>", "<upcase>" + currentSubstring.ToUpper() + "</upcase>");
+
+            start = indexClosingTag + 9;
+        }
+
+        return inputString;
+    }
+
+    static string ParseupcaseTagsWithRegex(string inputString)
+    {
+        string pattern = @"(<upcase>)(?'substring'[\w\s\d]+)(<\/upcase>)";
+        MatchCollection matches = Regex.Matches(inputString, pattern, RegexOptions.IgnoreCase);
+        foreach (Match match in matches)
+        {
+            inputString = inputString.Replace(match.Groups["substring"].Value, match.Groups["substring"].Value.ToUpper());
+        }
+
+        return inputString;
+    }
+
+    static void Main(string[] args)
+    {
+        string inputString = Console.ReadLine()!;
+
+        Console.WriteLine(ParseupcaseTagsIteratively(inputString));
+        Console.WriteLine(ParseupcaseTagsWithRegex(inputString));
+    }
+}
+
+/*
+
+You are given a text. Write a program that changes the text in all regions surrounded by the tags <upcase> and </upcase> to upper-case.
+The tags cannot be nested.
+Example: We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else.
+The expected result: We are living in a YELLOW SUBMARINE. We don't have ANYTHING else.
+
+*/
---
+++ String_length/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static string FillMissingCharsWithStars(string inputString)
+    {
+        string resultString = inputString;
+
+        if (String.IsNullOrEmpty(inputString))
+        {
+            resultString = "";
+            IEnumerable<string> stars = Enumerable.Repeat("*", 20);
+            foreach (String str in stars)
+            {
+                resultString += str;
+            }
+        }
+        
+        if (inputString.Length < 20)
+        {
+            IEnumerable<string> stars = Enumerable.Repeat("*", 20 - inputString.Length);
+            foreach (String str in stars)
+            {
+                resultString += str;
+            }
+        }
+
+        return resultString;
+    }
+
+    static void Main(string[] args)
+    {
+        string inputString = Console.ReadLine();
+
+        Console.WriteLine(FillMissingCharsWithStars(inputString));
+
+    }
+}
+
+/*
+
+Write a program that reads from the console a string of maximum 20 characters. If the length of the string is less than 20, the rest of the characters should be filled with *.
+Print the result string into the console.
+
+
+*/
